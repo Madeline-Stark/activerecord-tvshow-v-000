94c614972d433ef2f89c32107b0f567f5dd1c498
Madeline Stark <madeline.stark1@gmail.com>
2018-08-20 18:51:08 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,2 +1,37 @@
 class Show < ActiveRecord::Base
+
+  def highest_rating
+    #max value in ratings column
+  end
+
+  def most_popular_show
+    #use highest rating method to return show with highest rating
+  end
+
+  def lowest_rating
+    #use min method
+  end
+
+  def least_popular_show
+
+  end
+
+  def ratings_sum
+    #returns sum of all ratings
+  end
+
+  def popular_shows
+    #returns an array of all of the shows that have
+    #a rating greater than 5.
+    #hint: use the where Active Record method.
+  end
+
+  def shows_by_alphabetical_order
+    # returns an array of all of the shows sorted by
+    # alphabetical order according to their names.
+    # hint: use the order Active Record method.
+  end
+
+
+
 end
